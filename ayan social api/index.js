018ce226737ed6b6c6bc39d35bc742a72e99c6a4
AyanAcharya <ayan.pgec@gmail.com>
2023-03-19 19:07:38 +0530

    first commit

---
+++ ayan social api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+mongoose.connect('mongodb+srv://admin:U43CnTiyFOKoKSzs@cluster0.owjpftx.mongodb.net/app?retryWrites=true&w=majority')
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
